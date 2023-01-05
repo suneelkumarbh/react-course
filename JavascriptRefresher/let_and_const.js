afec5fd4a41fedce1696090d1259b1caee25d52f
Suneel Kumar <suneel.kumar@10pearls.com>
2023-01-05 16:09:00 +0500

    let and const

---
+++ JavascriptRefresher/let_and_const.js
@@ -0,0 +1,10 @@
+let myName = 'Suneel';
+console.log(myName);
+myName = 'Updated' // should be updated
+console.log(myName)
+
+
+// cont variables are assigned only once
+const myNameConst = 'Suneel';
+console.log(myNameConst)
+myNameConst = 'Update' // should throws an exception
