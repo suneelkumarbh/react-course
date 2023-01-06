8807750ce90733867cc291e893d76b83808d1b5d
Suneel Kumar <suneel.kumar@10pearls.com>
2023-01-06 22:08:46 +0500

    added multilines html in the return

--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -1,5 +1,17 @@
 function ExpenseItem(){
-  return <h2>ExpenseItem!</h2>
+  return (
+    <div>
+      <div>
+        March 28 2022
+      </div>
+      <div>
+        <h2>
+          Car Insurance
+        </h2>
+        <div>$294.7</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
