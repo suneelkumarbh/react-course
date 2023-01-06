d08e52f86fcdb14df021cf0c126b871f9213098c
Suneel Kumar <suneel.kumar@10pearls.com>
2023-01-06 22:42:16 +0500

    moved data to const variables and used them instead of hard coded in jsx

--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -1,16 +1,16 @@
 import './ExpenseItem.css'
 
 function ExpenseItem(){
+  const expenseDate = new Date(2022, 2, 28);
+  const expenseTitle = 'Car insurance';
+  const expenseAmount = 294.67;
+
   return (
     <div className = 'expense-item'>
-      <div>
-        March 28 2022
-      </div>
+      <div>{expenseDate.toISOString()}</div>
       <div className = 'expense-item__description'>
-        <h2>
-          Car Insurance
-        </h2>
-        <div className = 'expense-item__price'>$294.7</div>
+        <h2>{expenseTitle}</h2>
+        <div className = 'expense-item__price'>${expenseAmount}</div>
       </div>
     </div>
   );
