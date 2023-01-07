0be840882f37ea4b9756fd5276bc68bc96d38209
Suneel Kumar <suneel.kumar@10pearls.com>
2023-01-07 20:23:27 +0500

    removed temporary change title button

--- 01-starting-setup/src/components/Expenses/ExpenseItem.js
+++ 01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -1,27 +1,22 @@
-import React, {useState} from 'react';
-import './ExpenseItem.css'
-import ExpenseDate from './ExpenseDate'
-import Card from '../UI/Card';
+import React, { useState } from "react";
+import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
+import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
   // const expenseDate = new Date(2022, 2, 28);
   // const expenseTitle = 'Car insurance';
   // const expenseAmount = 294.67;
-  const [title, setTitle] = useState(props.title);
-  const clickHandler = () => {
-    setTitle('Updated!')
-  };
 
   return (
-    <Card className = 'expense-item'>
-      <ExpenseDate date={props.date}/>
-      <div className = 'expense-item__description'>
-        <h2>{title}</h2>
-        <div className = 'expense-item__price'>${props.amount}</div>
-        <button onClick={clickHandler}>Change title</button>
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </Card>
   );
-}
+};
 
 export default ExpenseItem;
