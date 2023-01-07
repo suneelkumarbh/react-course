1db480ef6ca636ed98970eb76ac3099b0304ed44
Suneel Kumar <suneel.kumar@10pearls.com>
2023-01-07 15:06:39 +0500

    used state to update the title

--- 01-starting-setup/src/components/Expenses/ExpenseItem.js
+++ 01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -1,3 +1,4 @@
+import React, {useState} from 'react';
 import './ExpenseItem.css'
 import ExpenseDate from './ExpenseDate'
 import Card from '../UI/Card';
@@ -6,15 +7,16 @@ const ExpenseItem = (props) => {
   // const expenseDate = new Date(2022, 2, 28);
   // const expenseTitle = 'Car insurance';
   // const expenseAmount = 294.67;
+  const [title, setTitle] = useState(props.title);
   const clickHandler = () => {
-    console.log('Clicked!!!!!');
+    setTitle('Updated!')
   };
 
   return (
     <Card className = 'expense-item'>
       <ExpenseDate date={props.date}/>
       <div className = 'expense-item__description'>
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className = 'expense-item__price'>${props.amount}</div>
         <button onClick={clickHandler}>Change title</button>
       </div>
