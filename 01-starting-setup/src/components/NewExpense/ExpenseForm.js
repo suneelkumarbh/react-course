1299451c38af803c2682f7bd629caa7626a250fa
Suneel Kumar <suneel.kumar@10pearls.com>
2023-01-07 16:39:22 +0500

    two way binding of all the fields

--- 01-starting-setup/src/components/NewExpense/ExpenseForm.js
+++ 01-starting-setup/src/components/NewExpense/ExpenseForm.js
@@ -25,6 +25,9 @@ const ExpenseForm = () => {
     };
 
     console.log(expeseData);
+    setEnteredAmount("");
+    setEnteredDate("");
+    setEnteredTitle("");
   };
 
   return (
@@ -32,7 +35,11 @@ const ExpenseForm = () => {
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={titleChangeHandler} />
+          <input
+            type="text"
+            value={enteredTitle}
+            onChange={titleChangeHandler}
+          />
         </div>
 
         <div className="new-expense__control">
@@ -41,6 +48,7 @@ const ExpenseForm = () => {
             type="number"
             min="0.01"
             step="0.01"
+            value={enteredAmount}
             onChange={amountChangeHandler}
           />
         </div>
@@ -51,6 +59,7 @@ const ExpenseForm = () => {
             type="date"
             min="2019-01-01"
             max="31-12-2023"
+            value={enteredDate}
             onChange={dateChangeHandler}
           />
         </div>
