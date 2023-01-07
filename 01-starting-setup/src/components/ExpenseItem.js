e1fbf7e98296c2836525e4ec2c5fe20329916a5a
Suneel Kumar <suneel.kumar@10pearls.com>
2023-01-07 12:22:19 +0500

    splitted the date component

--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -1,20 +1,14 @@
 import './ExpenseItem.css'
+import ExpenseDate from './ExpenseDate'
 
 function ExpenseItem(props){
   // const expenseDate = new Date(2022, 2, 28);
   // const expenseTitle = 'Car insurance';
   // const expenseAmount = 294.67;
-  const month = props.date.toLocaleString('en-US', { month: 'long' });
-  const day = props.date.toLocaleString('en-US', { day: '2-digit' });
-  const year = props.date.getFullYear();
 
   return (
     <div className = 'expense-item'>
-      <div>
-        <div>{month}</div>
-        <div>{year}</div>
-        <div>{day}</div>
-      </div>
+      <ExpenseDate date={props.date}/>
       <div className = 'expense-item__description'>
         <h2>{props.title}</h2>
         <div className = 'expense-item__price'>${props.amount}</div>
