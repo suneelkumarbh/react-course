68c518a840e4be11d9f073e9f28c579bde09e856
Suneel Kumar <suneel.kumar@10pearls.com>
2023-01-07 16:22:24 +0500

    update state that depends on previous state

--- 01-starting-setup/src/components/NewExpense/ExpenseForm.js
+++ 01-starting-setup/src/components/NewExpense/ExpenseForm.js
@@ -13,9 +13,16 @@ const ExpenseForm = () => {
   });
   const titleChangeHandler = (event) => {
     //setEnteredTitle(event.target.value);
-    setUserInput({
-      ...userInput,
-      enteredTitle: event.target.value,
+    // setUserInput({
+    //   ...userInput,
+    //   enteredTitle: event.target.value,
+    // });
+
+    setUserInput((prevState) => {
+      return {
+        ...prevState,
+        enteredTitle: event.target.value,
+      };
     });
   };
   const amountChangeHandler = (event) => {
