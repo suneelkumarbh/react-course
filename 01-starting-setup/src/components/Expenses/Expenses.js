51d869448302ba70650d4e22ca9f04daa655fb9c
Suneel Kumar <suneel.kumar@10pearls.com>
2023-01-08 23:25:12 +0500

    added key

--- 01-starting-setup/src/components/Expenses/Expenses.js
+++ 01-starting-setup/src/components/Expenses/Expenses.js
@@ -17,7 +17,12 @@ const Expenses = (props) => {
         onYearSelect={onYearSelectHandler}
       />
       {props.items.map((item) => (
-        <ExpenseItem title={item.title} amount={item.amount} date={item.date} />
+        <ExpenseItem
+          key={item.id}
+          title={item.title}
+          amount={item.amount}
+          date={item.date}
+        />
       ))}
     </Card>
   );
