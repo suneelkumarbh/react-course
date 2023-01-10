4194d8d21c9bc5c14744b4c9fd43bb962daafe08
Suneel Kumar <suneel.kumar@10pearls.com>
2023-01-10 19:09:58 +0500

    user input state management

--- 01-starting-project/src/components/UI/Card.js
+++ 01-starting-project/src/components/UI/Card.js
@@ -1,11 +1,6 @@
-import styled from "styled-components";
-const Card = (props) => {
-  const CardDiv = styled.div`
-    background: white;
-    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
-    border-radius: 10px;
-  `;
+import CardDiv from "./CardDiv";
 
+const Card = (props) => {
   return (
     <CardDiv className={props.className}>
       {props.children}
--- 01-starting-project/src/components/User/User.js
+++ 01-starting-project/src/components/User/User.js
@@ -2,20 +2,47 @@ import React from "react";
 import styles from "./User.module.css";
 import Card from "../UI/Card";
 import Button from "../UI/Button";
+import { useState } from "react";
 const User = (props) => {
+  const [enteredUserName, setEnteredUserName] = useState("");
+  const [enteredAge, setEnteredAge] = useState("");
+
   const onFormSubmitHandler = (event) => {
     event.preventDefault();
+    console.log(enteredUserName);
+    console.log(enteredAge);
+    setEnteredAge('');
+    setEnteredUserName('');
   };
+
+  const changeUserNameHandler = (event) => {
+    setEnteredUserName(event.target.value);
+  };
+
+  const changeNumberHandler = (event) => {
+    setEnteredAge(event.target.value);
+  };
+
   return (
     <Card className={styles.input}>
       <form onSubmit={onFormSubmitHandler}>
         <div>
           <label htmlFor="username">Username</label>
-          <input id="username" type="text" />
+          <input
+            id="username"
+            type="text"
+            onChange={changeUserNameHandler}
+            value={enteredUserName}
+          />
         </div>
         <div>
           <label htmlFor="age">Age(years)</label>
-          <input id="age" type="number" />
+          <input
+            id="age"
+            type="number"
+            onChange={changeNumberHandler}
+            value={enteredAge}
+          />
         </div>
         <div>
           <Button type="submit">Add User</Button>
