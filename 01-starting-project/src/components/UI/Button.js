2d3d43c5a080263f7b6fec98b31f70b835147cf0
Suneel Kumar <suneel.kumar@10pearls.com>
2023-01-10 18:48:23 +0500

    fallback of button

--- 01-starting-project/src/components/UI/Button.js
+++ 01-starting-project/src/components/UI/Button.js
@@ -2,7 +2,7 @@ import styles from "./Button.module.css";
 
 const Button = (props) => {
   return (
-    <button className={styles.button} type={props.type}>
+    <button className={styles.button} type={props.type || "button"}>
       {props.children}
     </button>
   );
