b4a81310fdd9e0b857bfe75b1c396f175a2418e5
Suneel Kumar <suneel.kumar@10pearls.com>
2023-01-11 11:41:13 +0500

    click outside the modal click handler

--- 01-starting-project/src/components/UI/ErrorModal.js
+++ 01-starting-project/src/components/UI/ErrorModal.js
@@ -6,7 +6,7 @@ import styles from "./ErrorModal.module.css";
 const ErrorModal = (props) => {
   return (
     <div>
-      <div className={styles.backdrop} />
+      <div className={styles.backdrop} onClick={props.onOkay}/>
       <Card className={styles.modal}>
         <header className={styles.header}>
           <h2>{props.error.title}</h2>
