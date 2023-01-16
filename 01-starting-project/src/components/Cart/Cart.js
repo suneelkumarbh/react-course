c2ef4817420db6cebd164e4582a8777b01dc1789
Suneel Kumar <suneel.kumar@10pearls.com>
2023-01-16 17:14:46 +0500

    added cart reducer

--- 01-starting-project/src/components/Cart/Cart.js
+++ 01-starting-project/src/components/Cart/Cart.js
@@ -1,9 +1,12 @@
 import classes from "./Cart.module.css";
 import Modal from "../UI/Modal";
+import { useContext } from "react";
+import CartContext from "../../store/cart-context";
 const Cart = (props) => {
+  const cartCtx = useContext(CartContext);
   const cartItems = (
     <ul className={classes["cart-items"]}>
-      {[{ id: "c1", name: "Sushi", amount: 2, price: 12.99 }].map((item) => {
+      {cartCtx.items.map((item) => {
         return <li key={item.id}>{item.name}</li>;
       })}
     </ul>
--- 01-starting-project/src/store/CartProvider.js
+++ 01-starting-project/src/store/CartProvider.js
@@ -1,13 +1,40 @@
+import { useReducer } from "react";
 import CartContext from "./cart-context";
+const defaultCartState = {
+  items: [],
+  totalAmount: 0,
+};
+const cartReducer = (state, action) => {
+  if (action.type === "ADD") {
+    const updatedItems = state.items.concat(action.item);
+    const updateTotalAmount =
+      state.totalAmount + action.item.price * action.item.amount;
+    return { items: updatedItems, totalAmount: updateTotalAmount };
+  } else if (action.type === "REMOVE") {
+  }
+  return defaultCartState;
+};
 const CartProvider = (props) => {
-  const addItemHandler = (item) => {};
-  const removeItemHandler = (id) => {};
+  const [cartState, dispatchCartState] = useReducer(
+    cartReducer,
+    defaultCartState
+  );
+
+  const addItemHandler = (item) => {
+    dispatchCartState({ type: "ADD", item: item });
+  };
+
+  const removeItemHandler = (id) => {
+    dispatchCartState({ type: "REMOVE", id: id });
+  };
+
   const cartContext = {
-    items: [],
-    totalAmount: 0,
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
     addItem: addItemHandler,
     removeItem: removeItemHandler,
   };
+
   return (
     <CartContext.Provider value={cartContext}>
       {props.children}
