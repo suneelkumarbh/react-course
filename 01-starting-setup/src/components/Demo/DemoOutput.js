e119a8d8e92ea8699076dcb14db1687a10f7add1
Suneel Kumar <suneel.kumar@10pearls.com>
2023-01-17 15:35:45 +0500

    react memo

--- 01-starting-setup/src/components/Demo/DemoOutput.js
+++ 01-starting-setup/src/components/Demo/DemoOutput.js
@@ -1,7 +1,8 @@
+import React from "react";
 import MyParagraph from "./MyParagraph";
 
 const DemoOutput = (props) => {
   console.log("DemoOutput running");
   return <MyParagraph>{props.show ? "This is new!" : ""}</MyParagraph>;
 };
-export default DemoOutput;
+export default React.memo(DemoOutput);
