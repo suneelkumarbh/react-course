446959222129d7ab490bf462ff1d26ff2e1bb138
Suneel Kumar <suneel.kumar@10pearls.com>
2023-01-17 14:58:23 +0500

    component update in action

--- 01-starting-setup/src/App.js
+++ 01-starting-setup/src/App.js
@@ -1,11 +1,18 @@
-import React from 'react';
-
-import './App.css';
+import React, { useState } from "react";
+import Button from "./components/UI/Button/Button";
+import "./App.css";
 
 function App() {
+  const [showParagraph, setShowParagraph] = useState(false);
+  console.log("App Running");
+  const btcClickHandler = () => {
+    setShowParagraph(() => !showParagraph);
+  };
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      {showParagraph && <p>This is the paragraph</p>}
+      <Button onClick={btcClickHandler}>Show Paragraph!</Button>
     </div>
   );
 }
