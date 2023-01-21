00ba934c33d2193462d84ac61ef6314e3369046d
Suneel Kumar <suneel.kumar@10pearls.com>
2023-01-22 01:18:59 +0500

    custom hook for http request

---
+++ custom-hooks/04-onwards-to-a-more-realistic-example/src/App.js
@@ -0,0 +1,44 @@
+import React, { useCallback, useEffect, useState } from "react";
+
+import Tasks from "./components/Tasks/Tasks";
+import NewTask from "./components/NewTask/NewTask";
+import useHttp from "./hooks/use-http";
+
+function App() {
+  const [tasks, setTasks] = useState([]);
+  const applyData = useCallback((data) => {
+    const loadedTasks = [];
+
+    for (const taskKey in data) {
+      loadedTasks.push({ id: taskKey, text: data[taskKey].text });
+    }
+
+    setTasks(loadedTasks);
+  }, []);
+
+  const { isLoading, error, sendRequest: fetchTasks } = useHttp(applyData);
+
+  useEffect(() => {
+    fetchTasks({
+      url: "https://react-http-b8814-default-rtdb.firebaseio.com/tasks.json",
+    });
+  }, [fetchTasks]);
+
+  const taskAddHandler = (task) => {
+    setTasks((prevTasks) => prevTasks.concat(task));
+  };
+
+  return (
+    <React.Fragment>
+      <NewTask onAddTask={taskAddHandler} />
+      <Tasks
+        items={tasks}
+        loading={isLoading}
+        error={error}
+        onFetch={fetchTasks}
+      />
+    </React.Fragment>
+  );
+}
+
+export default App;
