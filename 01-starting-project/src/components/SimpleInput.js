a73f2ccee108f631ec37404be7f826f1c77960b3
Suneel Kumar <suneel.kumar@10pearls.com>
2023-01-22 21:08:31 +0500

    blur and derived states

--- 01-starting-project/src/components/SimpleInput.js
+++ 01-starting-project/src/components/SimpleInput.js
@@ -1,31 +1,31 @@
-import { useRef, useState } from "react";
+import { useState } from "react";
 
 const SimpleInput = (props) => {
-  const nameInputRef = useRef();
   const [enteredName, setEnteredName] = useState("");
-  const [enteredNameIsValid, setEnteredNameIsValid] = useState(false);
   const [enteredNameTouched, setEnteredNameTouched] = useState(false);
+  const entereNameIsValid = enteredName !== ''
+  const enteretedNameIsInvalid = enteredNameTouched && !entereNameIsValid;
   const nameInputChangeHandler = (event) => {
     setEnteredName(event.target.value);
   };
+
+  const nameInputBlurHandler = (event) => {
+    setEnteredNameTouched(true);
+  };
+
   const formSubmitHandler = (event) => {
     event.preventDefault();
     setEnteredNameTouched(true);
 
-    if (enteredName === "") {
-      setEnteredNameIsValid(false);
+    if (enteretedNameIsInvalid) {
       return;
     }
 
-    setEnteredNameIsValid(true);
-    console.log(enteredName);
-    console.log(nameInputRef.current.value);
-
-    // nameInputRef.current. value  => Do not do this, as this changes the real DOM and its a vanila javascript
+    setEnteredNameTouched(false);
     setEnteredName("");
   };
 
-  const enteretedNameIsInvalid = enteredNameTouched && !enteredNameIsValid;
+  
   const nameInputClasses = enteretedNameIsInvalid
     ? "form-control invalid"
     : "form-control";
@@ -37,7 +37,7 @@ const SimpleInput = (props) => {
           type="text"
           id="name"
           onChange={nameInputChangeHandler}
-          ref={nameInputRef}
+          onBlur={nameInputBlurHandler}
           value={enteredName}
         />
         {enteretedNameIsInvalid && (
