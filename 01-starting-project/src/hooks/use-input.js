ed1f4b3d95e1ec14a5a7789e8b53320ad2967561
Suneel Kumar <suneel.kumar@10pearls.com>
2023-01-24 01:32:10 +0500

    used reducer for input state

--- 01-starting-project/src/hooks/use-input.js
+++ 01-starting-project/src/hooks/use-input.js
@@ -1,31 +1,51 @@
-import { useState } from "react";
+import { useReducer } from "react";
+
+const initialInputState = {
+  value: "",
+  isTouched: false,
+};
+
+const inputStateReducer = (state, action) => {
+  if (action.type === "CHANGE") {
+    return { value: action.value, isTouched: state.isTouched };
+  }
+  if (action.type === 'BLUR'){
+    return { value: state.value, isTouched: true };
+  }
+  if (action.type === 'RESET'){
+    return {value: '', isTouched: false}
+  }
+  return initialInputState;
+};
 const useInput = (validateInput) => {
-  const [field, setField] = useState("");
-  const [fieldIsTouched, setFieldIsTouched] = useState(false);
-  const fieldIsValid = validateInput(field);
-  const fieldIsInvalid = !fieldIsValid && fieldIsTouched;
+  const [inputState, dispatchInputState] = useReducer(
+    inputStateReducer,
+    initialInputState
+  );
+  const fieldIsValid = validateInput(inputState.value);
+  const fieldIsInvalid = !fieldIsValid && inputState.isTouched;
 
   const fieldChangeHandler = (event) => {
-    setFieldIsTouched(true);
-    setField(event.target.value);
+    dispatchInputState({value: event.target.value, type: 'CHANGE'})
   };
 
   const fieldBlurHandler = (event) => {
-    setFieldIsTouched(true);
+    dispatchInputState({type: 'BLUR'})
   };
 
   const resetFieldHandler = () => {
-    setFieldIsTouched(false);
-    setField("");
+    dispatchInputState({
+      type: 'RESET'
+    })
   };
 
   return {
-    value: field,
+    value: inputState.value,
     fieldIsInvalid,
     fieldIsValid,
     fieldChangeHandler,
     fieldBlurHandler,
-    resetFieldHandler
+    resetFieldHandler,
   };
 };
 
