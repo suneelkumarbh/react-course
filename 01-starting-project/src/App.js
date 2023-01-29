76d56afd22be3ab106e38eba26de58bf62ac55ea
Suneel Kumar <suneel.kumar@10pearls.com>
2023-01-29 18:01:27 +0500

    http request with useEffect()

--- 01-starting-project/src/App.js
+++ 01-starting-project/src/App.js
@@ -2,9 +2,18 @@ import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 import { useSelector } from "react-redux";
+import { useEffect } from "react";
 
 function App() {
   const cartIsVisible = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    fetch(
+      "https://react-http-b8814-default-rtdb.firebaseio.com/redux-cart.json",
+      { method: "PUT", body: JSON.stringify(cart) }
+    );
+  }, [cart]);
   return (
     <Layout>
       {cartIsVisible && <Cart />}
