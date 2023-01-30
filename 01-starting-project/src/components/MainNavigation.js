6efecc9b2d7719ff2776c6fbf77901120d829e83
Suneel Kumar <suneel.kumar@10pearls.com>
2023-01-30 17:04:23 +0500

    navlinks

--- 01-starting-project/src/components/MainNavigation.js
+++ 01-starting-project/src/components/MainNavigation.js
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import classes from "./MainNavigation.module.css";
 const MainNavigation = (props) => {
   return (
@@ -6,10 +6,25 @@ const MainNavigation = (props) => {
       <nav>
         <ul className={classes.list}>
           <li>
-            <Link to="/">Home</Link>
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+              end
+            >
+              Home
+            </NavLink>
           </li>
           <li>
-            <Link to="/products">Products</Link>
+            <NavLink
+              to="/products"
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+            >
+              Products
+            </NavLink>
           </li>
         </ul>
       </nav>
