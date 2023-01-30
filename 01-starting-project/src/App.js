a02c5f6074f95f2519474ae6d9c18bc9d3453619
Suneel Kumar <suneel.kumar@10pearls.com>
2023-01-30 15:42:06 +0500

    basic routing functionality for home page and products page

--- 01-starting-project/src/App.js
+++ 01-starting-project/src/App.js
@@ -1,5 +1,13 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomePage from "./components/HomePage";
+import Products from "./components/Products";
+
+const router = createBrowserRouter([
+  { path: "/", element: <HomePage /> },
+  { path: "/products", element: <Products /> },
+]);
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
