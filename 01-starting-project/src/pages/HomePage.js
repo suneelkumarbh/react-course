068089f2cfff284aebc6035d9116a3a124227ee8
Suneel Kumar <suneel.kumar@10pearls.com>
2023-01-30 17:14:53 +0500

    navigating programatically

--- 01-starting-project/src/pages/HomePage.js
+++ 01-starting-project/src/pages/HomePage.js
@@ -1,11 +1,16 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 function HomePage() {
+  const navigate = useNavigate();
+  const navigateHandler = () => {
+    navigate("/products");
+  };
   return (
     <>
       <h1>My Home Page</h1>
       <p>
         Go to <Link to="/products">list of products</Link>
       </p>
+      <button onClick={navigateHandler}>Products</button>
     </>
   );
 }
