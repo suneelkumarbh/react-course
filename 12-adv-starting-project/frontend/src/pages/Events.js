ec430f896fbd8939c61048a969e09837aad91cd8
Suneel Kumar <suneel.kumar@10pearls.com>
2023-01-30 21:59:34 +0500

    moved loader code to events

--- 12-adv-starting-project/frontend/src/pages/Events.js
+++ 12-adv-starting-project/frontend/src/pages/Events.js
@@ -7,3 +7,13 @@ function EventsPage() {
 }
 
 export default EventsPage;
+
+export const loader = async () => {
+  const response = await fetch("http://localhost:8080/events");
+
+  if (!response.ok) {
+  } else {
+    const resData = await response.json();
+    return resData.events;
+  }
+};
