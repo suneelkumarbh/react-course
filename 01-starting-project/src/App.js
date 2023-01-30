e2c3f920dcb56e3bc3a65a6df0457405237175eb
Suneel Kumar <suneel.kumar@10pearls.com>
2023-01-30 16:38:07 +0500

    layout and nested pages

--- 01-starting-project/src/App.js
+++ 01-starting-project/src/App.js
@@ -1,10 +1,17 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import HomePage from "./components/HomePage";
-import Products from "./components/Products";
+import HomePage from "./pages/HomePage";
+import Products from "./pages/Products";
+import RootLayout from "./pages/Root";
 
 const router = createBrowserRouter([
-  { path: "/", element: <HomePage /> },
-  { path: "/products", element: <Products /> },
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      { path: "/", element: <HomePage /> },
+      { path: "/products", element: <Products /> },
+    ],
+  },
 ]);
 function App() {
   return <RouterProvider router={router} />;
