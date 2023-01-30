719ee122b990fcc3aa38fdcf97046d74a46d38f5
Suneel Kumar <suneel.kumar@10pearls.com>
2023-01-30 16:52:12 +0500

    error page when route did not find

--- 01-starting-project/src/App.js
+++ 01-starting-project/src/App.js
@@ -1,4 +1,5 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ErrorPage from "./pages/Error";
 import HomePage from "./pages/HomePage";
 import Products from "./pages/Products";
 import RootLayout from "./pages/Root";
@@ -7,6 +8,7 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <RootLayout />,
+    errorElement: <ErrorPage />,
     children: [
       { path: "/", element: <HomePage /> },
       { path: "/products", element: <Products /> },
