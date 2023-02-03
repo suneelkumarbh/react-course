0f98f7f76b96466aec2adc153a9278aca0f858bb
Suneel Kumar <suneel.kumar@10pearls.com>
2023-02-03 13:49:18 +0500

    delete event

--- 12-adv-starting-project/frontend/src/pages/EventDetail.js
+++ 12-adv-starting-project/frontend/src/pages/EventDetail.js
@@ -1,4 +1,4 @@
-import { json, useRouteLoaderData } from "react-router-dom";
+import { json, redirect, useRouteLoaderData } from "react-router-dom";
 import EventItem from "../components/EventItem";
 
 const EventDetailPage = (props) => {
@@ -14,6 +14,19 @@ export async function loader({ request, params }) {
     throw json({ message: "Could not fetch record" }, { status: 500 });
   }
   return response;
+}
+
+export async function action({ params, request }) {
+  const id = params.id;
+  const response = await fetch("http://localhost:8080/events/" + id, {
+    method: request.method,
+  });
+
+  if (!response.ok) {
+    throw json({ message: "Could not delete record" }, { status: 500 });
+  }
+
+  return redirect("/events");
 }
 
 export default EventDetailPage;
