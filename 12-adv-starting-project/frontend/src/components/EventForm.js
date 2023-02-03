90d66d0b4c05be832f72b756b860e4dca5d6b536
Suneel Kumar <suneel.kumar@10pearls.com>
2023-02-03 13:57:31 +0500

    feedback to user when submitting

--- 12-adv-starting-project/frontend/src/components/EventForm.js
+++ 12-adv-starting-project/frontend/src/components/EventForm.js
@@ -1,9 +1,12 @@
-import { Form, useNavigate } from "react-router-dom";
+import { Form, useNavigate, useNavigation } from "react-router-dom";
 
 import classes from "./EventForm.module.css";
 
 function EventForm({ method, event }) {
   const navigate = useNavigate();
+  const navigation = useNavigation();
+
+  const isSubmitting = navigation.state === "submitting";
   function cancelHandler() {
     navigate("..");
   }
@@ -54,7 +57,9 @@ function EventForm({ method, event }) {
         <button type="button" onClick={cancelHandler}>
           Cancel
         </button>
-        <button>Save</button>
+        <button disabled={isSubmitting}>
+          {isSubmitting ? "Submitting" : "Save"}
+        </button>
       </div>
     </Form>
   );
