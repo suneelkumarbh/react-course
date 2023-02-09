4c3a80b43d4988a54d2b6ad49b86c38baf73983a
Suneel Kumar <suneel.kumar@10pearls.com>
2023-02-09 21:15:42 +0500

    wrapping transition and animation timing

--- 03-using-css-animations/src/components/Modal/Modal.js
+++ 03-using-css-animations/src/components/Modal/Modal.js
@@ -1,24 +1,39 @@
 import React from "react";
 
 import "./Modal.css";
+import { Transition } from "react-transition-group";
 
+const animationTiming = {
+  enter: 400,
+  exit: 1000,
+};
 const modal = (props) => {
-  const cssClasses = [
-    "Modal",
-    props.show === "entering"
-      ? "ModalOpen"
-      : props.show === "exiting"
-      ? "ModalClose"
-      : null,
-  ];
-
   return (
-    <div className={cssClasses.join(" ")}>
-      <h1>A Modal</h1>
-      <button className="Button" onClick={props.closed}>
-        Dismiss
-      </button>
-    </div>
+    <Transition
+      in={props.show}
+      timeout={animationTiming}
+      mountOnEnter
+      unmountOnExit
+    >
+      {(state) => {
+        const cssClasses = [
+          "Modal",
+          state === "entering"
+            ? "ModalOpen"
+            : state === "exiting"
+            ? "ModalClosed"
+            : null,
+        ];
+        return (
+          <div className={cssClasses.join(" ")}>
+            <h1>A Modal</h1>
+            <button className="Button" onClick={props.closed}>
+              Dismiss
+            </button>
+          </div>
+        );
+      }}
+    </Transition>
   );
 };
 
