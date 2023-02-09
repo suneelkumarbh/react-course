7423b23204db60ac268f8bd00cc616f58d04d5bb
Suneel Kumar <suneel.kumar@10pearls.com>
2023-02-09 20:55:31 +0500

    transition

---
+++ 03-using-css-animations/src/App.js
@@ -0,0 +1,77 @@
+import React, { Component } from "react";
+
+import "./App.css";
+import Modal from "./components/Modal/Modal";
+import Backdrop from "./components/Backdrop/Backdrop";
+import List from "./components/List/List";
+import { Transition } from "react-transition-group";
+
+class App extends Component {
+  state = {
+    modalIsOpen: false,
+    showBlock: false,
+  };
+
+  showModal = () => {
+    this.setState({ modalIsOpen: true });
+  };
+
+  closeModal = () => {
+    this.setState({ modalIsOpen: false });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>React Animations</h1>
+        <button
+          className="Button"
+          onClick={() =>
+            this.setState((prev) => ({ showBlock: !prev.showBlock }))
+          }
+        >
+          Toggle
+        </button>
+        <br />
+        <Transition
+          in={this.state.showBlock}
+          timeout={300}
+          mountOnEnter
+          unmountOnExit
+        >
+          {(state) => (
+            <div
+              style={{
+                backgroundColor: "red",
+                width: 100,
+                height: 100,
+                margin: "auto",
+                transition: "opacity 1s ease-out",
+                opacity: state === "exited" ? 0 : 1,
+              }}
+            ></div>
+          )}
+        </Transition>
+
+        <Transition
+          in={this.state.modalIsOpen}
+          timeout={300}
+          mountOnEnter
+          unmountOnExit
+        >
+          {(state) => <Modal show={state} closed={this.closeModal} />}
+        </Transition>
+        {this.state.modalIsOpen ? (
+          <Backdrop show={this.state.modalIsOpen} />
+        ) : null}
+        <button className="Button" onClick={this.showModal}>
+          Open Modal
+        </button>
+        <h3>Animating Lists</h3>
+        <List />
+      </div>
+    );
+  }
+}
+
+export default App;
