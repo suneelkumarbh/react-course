f9d1812213d5584b739be73e70e3339794dd64f7
Suneel Kumar <suneel.kumar@10pearls.com>
2023-02-10 14:43:42 +0500

    css transition

--- 03-using-css-animations/src/components/Modal/Modal.js
+++ 03-using-css-animations/src/components/Modal/Modal.js
@@ -1,15 +1,24 @@
 import React from "react";
 
 import "./Modal.css";
-import { Transition } from "react-transition-group";
+import { CSSTransition } from "react-transition-group";
 
 const animationTiming = {
   enter: 400,
   exit: 1000,
 };
 const modal = (props) => {
+  // const cssClasses = [
+  //   "Modal",
+  //   state === "entering"
+  //     ? "ModalOpen"
+  //     : state === "exiting"
+  //     ? "ModalClosed"
+  //     : null,
+  // ];
+
   return (
-    <Transition
+    <CSSTransition
       in={props.show}
       timeout={animationTiming}
       mountOnEnter
@@ -20,26 +29,15 @@ const modal = (props) => {
       onExit={() => console.log("onExit")}
       onExiting={() => console.log("onExiting")}
       onExited={() => console.log("onExited")}
+      classNames="fade-slide"
     >
-      {(state) => {
-        const cssClasses = [
-          "Modal",
-          state === "entering"
-            ? "ModalOpen"
-            : state === "exiting"
-            ? "ModalClosed"
-            : null,
-        ];
-        return (
-          <div className={cssClasses.join(" ")}>
-            <h1>A Modal</h1>
-            <button className="Button" onClick={props.closed}>
-              Dismiss
-            </button>
-          </div>
-        );
-      }}
-    </Transition>
+      <div className="Modal">
+        <h1>A Modal</h1>
+        <button className="Button" onClick={props.closed}>
+          Dismiss
+        </button>
+      </div>
+    </CSSTransition>
   );
 };
 
